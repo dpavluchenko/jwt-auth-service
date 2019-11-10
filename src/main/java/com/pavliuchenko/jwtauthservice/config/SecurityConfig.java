0ec1f53bc8bcd4531f3820f0f74f02cf312ee33c
dmitriy_pavluchenko <dmitriy1403199090@gmail.com>
2019-11-10 17:05:45 +0200

    changed converter for basic auth

--- src/main/java/com/pavliuchenko/jwtauthservice/config/SecurityConfig.java
+++ src/main/java/com/pavliuchenko/jwtauthservice/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.pavliuchenko.jwtauthservice.config;
 
 import com.pavliuchenko.jwtauthservice.config.auth.basic.BasicAuthSuccessHandler;
+import com.pavliuchenko.jwtauthservice.config.auth.basic.FormLoginAuthenticationConverter;
 import com.pavliuchenko.jwtauthservice.config.auth.jwt.JwtReactiveAuthenticationManager;
 import com.pavliuchenko.jwtauthservice.config.auth.jwt.JwtToAuthenticationConverter;
 import com.pavliuchenko.jwtauthservice.service.jwt.JwtService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
@@ -16,12 +17,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.authentication.ServerFormLoginAuthenticationConverter;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.web.server.WebFilter;
 
 @Configuration
 @EnableWebFluxSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final ReactiveUserDetailsService userDetailsService;
@@ -30,7 +31,6 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .csrf().disable()
                 .authorizeExchange()
                 .pathMatchers("/sign-up","/service/public").permitAll()
                 .and()
@@ -40,10 +40,11 @@ public class SecurityConfig {
                 .and()
                 .addFilterAt(createBasicFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
                 .authorizeExchange()
-                .pathMatchers("/user-info")
+                .pathMatchers(getJwtPatterns())
                 .authenticated()
                 .and()
-                .addFilterAt(createJwtAuthFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
+                .addFilterAt(createJwtAuthFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
+                .csrf().disable();
         return http.build();
     }
 
@@ -56,7 +57,7 @@ public class SecurityConfig {
         UserDetailsRepositoryReactiveAuthenticationManager manager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
         manager.setPasswordEncoder(passwordEncoder());
         AuthenticationWebFilter filter = new AuthenticationWebFilter(manager);
-        filter.setServerAuthenticationConverter(new ServerFormLoginAuthenticationConverter());
+        filter.setServerAuthenticationConverter(new FormLoginAuthenticationConverter());
         filter.setAuthenticationSuccessHandler(new BasicAuthSuccessHandler(jwtService));
         return filter;
     }
@@ -65,7 +66,12 @@ public class SecurityConfig {
         JwtReactiveAuthenticationManager manager = new JwtReactiveAuthenticationManager();
         AuthenticationWebFilter filter = new AuthenticationWebFilter(manager);
         filter.setServerAuthenticationConverter(new JwtToAuthenticationConverter(jwtService));
+        filter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers(getJwtPatterns()));
         return filter;
+    }
+
+    private String[] getJwtPatterns(){
+        return new String[]{"/user-info"};
     }
 
 }

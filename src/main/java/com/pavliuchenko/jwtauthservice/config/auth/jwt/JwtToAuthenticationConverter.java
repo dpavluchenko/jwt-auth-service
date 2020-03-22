6616bdbb80e2743c4a5bb2f21318ecbaddadd7cb
dpavliuchenko <dpavliuchenko@ancestry.com>
2020-03-22 15:45:39 +0200

    changed exception for bad jwt

--- src/main/java/com/pavliuchenko/jwtauthservice/config/auth/jwt/JwtToAuthenticationConverter.java
+++ src/main/java/com/pavliuchenko/jwtauthservice/config/auth/jwt/JwtToAuthenticationConverter.java
@@ -1,13 +1,14 @@
 package com.pavliuchenko.jwtauthservice.config.auth.jwt;
 
 import com.pavliuchenko.jwtauthservice.domain.JwtInfo;
-import com.pavliuchenko.jwtauthservice.exception.JwtNotValidException;
 import com.pavliuchenko.jwtauthservice.service.jwt.JwtService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -20,7 +21,7 @@ public class JwtToAuthenticationConverter implements ServerAuthenticationConvert
     public Mono<Authentication> convert(ServerWebExchange exchange) {
         return jwtService
                 .validateToken(exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
-                .switchIfEmpty(Mono.defer(() -> Mono.error(new JwtNotValidException("JWT isn't valid!"))))
+                .switchIfEmpty(Mono.defer(() -> Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "JWT isn't valid!"))))
                 .map(JwtInfo::toUser)
                 .map(user -> new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
     }
